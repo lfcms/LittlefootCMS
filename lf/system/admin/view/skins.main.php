ef2dbbd854ae997d2200469f787e949d014e06ca
Mike <dev@dev.eflipdomains.com>
2013-12-07 19:35:54 -0500

    went a different direction with the skins page

--- lf/system/admin/view/skins.main.php
+++ lf/system/admin/view/skins.main.php
@@ -1,21 +1,21 @@
-<div id="new_skins" class="skin_page_container">
-	<h3>New Skins (<a href="%appurl%download/">Store</a>)</h3>
-	<h4>Upload</h4>
-	<form id="upload_skin_form" enctype="multipart/form-data" action="%appurl%install/" method="post">
-		<input type="hidden" name="MAX_FILE_SIZE" value="55000000" />
-		<p><input id="upload_skin_file" type="file" name="skin" /></p>
-		<p><?php echo $install; ?> (<?php echo ini_get('post_max_size'); ?>/<?php echo ini_get('upload_max_filesize'); ?> Upload Limit)</p>
-	</form>
-	<h4>Create</h4>
-	<form id="create_skin_form" action="%appurl%blankskin/" method="post">
-		<input id="create_skin_namebox" type="text" name="name" placeholder="Name your new skin" />
-		<input type="submit" value="Create it" />
-	</form> 
-</div>
 <div id="avail_skins" class="skin_page_container">
-	<h3>Available Skins:</h3>
+	<h2>Skins Manager</h2>
 	<p>Click a skin to set it as the default. [x] to remove a skin. Zip to download a skin.</p>
 	<ul class="applist">
+	<li id="new_skins">
+		<h3>New Skin (<a href="%appurl%download/">Store</a>)</h3>
+		<h4>Upload</h4>
+		<form id="upload_skin_form" enctype="multipart/form-data" action="%appurl%install/" method="post">
+			<input type="hidden" name="MAX_FILE_SIZE" value="55000000" />
+			<p><input id="upload_skin_file" type="file" name="skin" /></p>
+			<p><?php echo $install; ?> (<?php echo ini_get('post_max_size'); ?>/<?php echo ini_get('upload_max_filesize'); ?> Upload Limit)</p>
+		</form>
+		<h4>Create</h4>
+		<form id="create_skin_form" action="%appurl%blankskin/" method="post">
+			<input id="create_skin_namebox" type="text" name="name" placeholder="Name your new skin" />
+			<input type="submit" value="Create it" />
+		</form> 
+	</li>
 	<?php
 	foreach(scandir($pwd) as $file)
 	{
@@ -30,7 +30,7 @@
 		if(is_file($skin.'/index.php'))
 		{
 			?>
-			<li style="padding: 5px; <?php echo $highlight; ?>"> 
+			<li style="padding: 15px; <?php echo $highlight; ?>"> 
 						<div class="left_header">
 							<a onclick="return confirm('Do you really want to delete this?');" href="%appurl%rm/<?=$file;?>/">x</a>
 						</div>
