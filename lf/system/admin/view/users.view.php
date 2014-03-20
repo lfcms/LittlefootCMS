ee11de81eb14002f630603b183bc7b2c72d0d587
Mike <dev@dev.animalvsmachine.com>
2014-03-19 21:32:25 -0400

    'users looks better too'

--- lf/system/admin/view/users.view.php
+++ lf/system/admin/view/users.view.php
@@ -1,7 +1,7 @@
 <h2>Users (<?=$count;?>) <a href="%appurl%newuser/">Create New</a></h2>
 <table cellspacing=0 cellpadding=5 style="width: 100%;">
-	<tr style="text-align:left;">
-		<th><?=$headers;?></th
+	<tr>
+		<th><?=$headers;?></th>
 		<th></th>
 		<th></th>
 	</tr>
