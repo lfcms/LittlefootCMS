fab496b03ce88577b10379691591eb8366295b45
Joe <joe@bioshazard.com>
2014-08-02 15:13:47 -0400

    'warning'

--- lf/system/admin/model/templateselect.php
+++ lf/system/admin/model/templateselect.php
@@ -17,9 +17,17 @@ if($match_file == 'default')
 	$skin = $pwd.'/'.$this->request->settings['default_skin'].'/index.php';
 	
 	// Get all %replace% keywords for selected template (remove system variables)
-	$template = file_get_contents($skin);
-	preg_match_all("/%([a-z]+)%/", str_replace(array('%baseurl%', '%skinbase%', '%nav%', '%title%'), '', $template), $tokens);
-	$section_list = $tokens[1];
+	if(!is_file($skin))
+	{
+		echo 'Currently selected skin does not exist. Please see the Skins tab to correct this.';
+		$section_list = array('none');
+	}
+	else
+	{
+		$template = file_get_contents($skin);
+		preg_match_all("/%([a-z]+)%/", str_replace(array('%baseurl%', '%skinbase%', '%nav%', '%title%'), '', $template), $tokens);
+		$section_list = $tokens[1];
+	}
 }
 
 $template_select .= ' value="default">Default</option>';
