db34a7594b01dfe8089eb7e4820fa105a3ac290c
Joe <joe@bioshazard.com>
2014-08-10 14:59:36 -0400

    'save'

--- lf/system/init.php
+++ lf/system/init.php
@@ -1,7 +1,5 @@
 <?php
 
-error_reporting(0); // my orm situation upsets the strict PHP standards. I will remove this after I fix that :P
-
 // Session name needs to be alphanumeric, just MD5 it to keep it unique and to not show the docroot
 session_name(md5(ROOT.$_SERVER['SERVER_NAME']));
 session_start(); 
@@ -12,6 +10,9 @@ include 'system/app.class.php';
 include 'system/lib/orm.php';
 include 'system/lib/recovery/install.php';
 include 'system/littlefoot.php';
+
+if(file_get_contents(ROOT.'system/version') != '1-DEV')
+	error_reporting(0); // my orm stuff upsets the strict PHP standards. I will remove this after I fix that :P
 
 if(!is_file('config.php')) 	install::noconfig();
 else 						include 'config.php'; // load db config
