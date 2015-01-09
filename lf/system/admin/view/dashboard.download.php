7033e57688b7d6fc77c0be74001c08fefc97cc5c
Joe <joe@bioshazard.com>
2015-01-08 20:25:00 -0500

    'whoops... forgot the new view for download'

---
+++ lf/system/admin/view/dashboard.download.php
@@ -0,0 +1,16 @@
+<h2><a href="%appurl%">Apps</a> / Download</h2>
+<div id="store-wrapper">
+	<p>Applications with a link can be installed. Those that are not links are already installed.</p>
+	
+	<ul>
+	<?php foreach($apps as $app => $ignore): ?> 	
+		<li>
+		<?php if(!isset($files[$app])): ?>
+			<a href="%appurl%getappfromnet/<?=$app;?>/"><?=$app;?></a>
+		<?php else: ?>
+			<?=$app;?> [<a href="%appurl%getappfromnet/<?=$app;?>/update/">Update</a>]
+		</li>
+		<?php endif; ?>
+	 <?php endforeach; ?>
+	</ul>
+</div>
