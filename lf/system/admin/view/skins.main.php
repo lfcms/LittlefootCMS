d9904b9e7e987d6aa3c65236464f121957d9b704
Joe <joe@bioshazard.com>
2015-03-01 14:01:18 -0500

    'readme implemented'

--- lf/system/admin/view/skins.main.php
+++ lf/system/admin/view/skins.main.php
@@ -37,32 +37,12 @@
 			</div>
 		</div>
 	</div>
-	
 
 	<!-- Installed Skins -->
 	<div class="col-9">
 		<div class="row spaced no_martop">
-
-		<?php $counter = 0; 
-
-		/*$imglist = array(
-			'http://cdn.tripwiremagazine.com/wp-content/uploads/2012/11/church-website-templates1.jpg',
-			'http://www.webdesignme.com/wp-content/uploads/revolution-theme-blue.jpg',
-			'http://blog.tmimgcdn.com/wp-content/uploads/2010/12/Free-Website-Template.jpg?7e70d4',
-			'http://blog.tmimgcdn.com/wp-content/uploads/2011/09/Free-Website-Template2.jpg?7e70d4',
-			'http://www.enjin.com/images/games/ffxiv/ffxiv-website-themes.jpg',
-			'http://www.opendesigns.org/od/wp-content/designs/34/34854/thumbnail.jpg'
-		);*/
-
-
-			foreach($skins as $skin): 
-			
-				
-		if($counter > 2 && $counter % 2 == 0): ?>
-
-		<?php endif;
-				
-				
+		<?php foreach($skins as $skin):
+					
 			$highlight = '';
 			if($skin == $request->settings['default_skin'])
 				$highlight = 'selected';
@@ -71,6 +51,10 @@
 				$screenshot = '%relbase%lf/skins/'.$skin.'/screenshot.png';
 			else
 				$screenshot = 'http://placehold.it/350x500';
+				// need to replace with local default image
+				
+			$readme = is_file(ROOT.'skins/'.$skin.'/readme') ? file_get_contents(ROOT.'skins/'.$skin.'/readme') : 'No readme found.';
+		
 		?>
 			<div class="col-6">
 				<div class="tile rounded <?=$highlight;?>">
@@ -79,35 +63,14 @@
 						<h4><?=$skin;?> <a onclick="return confirm('Do you really want to delete this?');" href="%appurl%rm/<?=$skin;?>/" class="x pull-right">x</a></h4>
 					</div>
 					
-					<!-- Screenshot Example 1-->
 					<div class="h250">
 						<a href="<?=$screenshot;?>"><img class="fit" src="<?=$screenshot;?>"  /></a>
 					</div>
 					
-					<!-- Screenshot Example 2-->
-						
-					<!--
-					
-					<?php if(is_file($skin.'/screenshot.png')): ?>
-					<div class="screenshot">
-						<a href="%relbase%lf/skins/<?=$skin;?>/screenshot.png" target="_blank">
-						<img src="%relbase%lf/skins/<?=$skin;?>/screenshot.png" alt="screenshot" />
-						</a>
-					</div>					
-					
-					
-					<?php else: ?>
-						<div class="screenshot">
-							<span>no screenshot available</span>
-						</div>
-					<?php endif; ?>
-					
-					-->
-					
 					<div class="tile-content">
 						<div class="row">
 							<div class="col-12">
-								<p class="h100 scroll no_mar">Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. Details and stuff I know you like these skins yo check it out. </p>
+								<p class="h100 scroll no_mar"><?=$readme;?></p>
 							</div>
 						</div>
 						<div class="row">
@@ -124,7 +87,7 @@
 					</div>
 				</div>
 			</div>
-			<?php endforeach; ?>
+		<?php endforeach; ?>
 		</div>
 	</div>
 </div>
