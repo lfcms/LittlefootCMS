977cf87ab0c6748794a7cd39ba77d7d4c27f60d0
Joe Still <joe@bioshazard.com>
2015-03-15 12:34:25 -0400

    'unified hidden and non hidden nav view'

--- lf/system/admin/view/dashboard-partial-hidden.php
+++ lf/system/admin/view/dashboard-partial-hidden.php
@@ -6,48 +6,7 @@ if(count($actions)):
 		$apps = $this->links[$action['id']];
 		$theapp = $apps[0]['app']; // support multi app linking... not in use atm
 	
-	?>
-	
-	<a id="nav_<?=$action['id'];?>"></a>
-	<div class="tile rounded<?=$this->edit==$action['id']?' active':'';?>">
-		<div class="tile-header gray_fg">
-			<div class="row">
-				<div class="col-9">
-					<!-- <?=$action['position'];?>. -->
-					(hidden)
-					<a href="%appurl%main/<?=$action['id'];?>/#nav_<?=$action['id'];?>">
-						<?=$action['label'];?>
-					</a> 
-				</div>
-				<div class="col-2">
-					<?php if( is_file(ROOT.'apps/'.$theapp.'/admin.php')): ?>
-					
-					<a href="%baseurl%apps/<?=$theapp;?>/"  class="pull-right">admin</a>
-								
-					<?php else: ?>
-							
-					<span class="pull-right">admin</span>
-								
-					<?php endif; ?>
-				</div>
-				<div class="col-1">
-					<a class="x pull-right" <?=jsprompt('Are you sure?');?> href="%baseurl%apps/rm/<?=$action['id'];?>/">x</a>
-				</div>
-			</div>
-		</div>
-		<?php if($this->edit == $action['id']): /* Load form if selected */ ?>
-		<div class="tile-content">
-			
-				<?=$this->partial('dashboard-partial-editform', array('save' => $action));?>
-				
-			
-			
-		</div>
-		<?php endif; ?>
-	</div>	
-	
-	<?php
-	
+		include 'view/dashboard-sharednavitem.php';
 	/* ?>
 	
 	<li>
