1ba840c41f787dc54b43859f24c909d4eccb3141
Joe Still <joe@bioshazard.com>
2015-04-20 23:23:31 -0400

    'copy index functionality'

--- lf/system/init.php
+++ lf/system/init.php
@@ -1,1 +1,7 @@
-<?php include dirname(__FILE__).'/bootstrap.php'; // init.php is deprecated, but this is an easy solution for backward compatibility to OOOLLLDDD installations.
+<?php 
+
+// init.php is deprecated, but this is an easy solution for backward compatibility to OOOLLLDDD installations.
+
+include dirname(__FILE__).'/bootstrap.php'; 
+$lf = new LittleFoot(); // initialize $lf with $db connection
+$lf->cms(); // execute littlefoot as ->cms() and ->render() output
