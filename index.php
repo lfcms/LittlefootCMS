3e654d3ebd574b7b0fa539663b7cc14898697d49
Joe Still <joe@bioshazard.com>
2015-05-14 16:53:55 -0400

    'whoops... messed up index'

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
 require_once('lf/system/bootstrap.php'); // include lf library
 
-$action = (new LfActions)->find();
-
-foreach($action as $k => $v)
-	echo $k.' = '.implode(',', array_values($v)).'<br />';
-
-
-
 // instantiate this way so the bootstrap can check the version without a parse error. 
 // should just be (new Littlefoot)->cms();
 $lf = new LittleFoot();
