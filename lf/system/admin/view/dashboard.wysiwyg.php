c14125e3be8d34443f31914d1d79a960c0f2975b
Joe Still <joe@bioshazard.com>
2015-07-30 22:30:55 -0400

    'nice'

--- lf/system/admin/view/dashboard.wysiwyg.php
+++ lf/system/admin/view/dashboard.wysiwyg.php
@@ -1,4 +1,49 @@
 <div class="row">
+	<div class="col-1">
+		Position: <input type="number" name="position" value="<?=$save['position'];?>" />
+	</div>
+	<div class="col-2">
+		Label: <input type="text" name="label" value="<?=$save['label'];?>" />	
+	</div>
+	<div class="col-3">
+		Title: <input type="text" name="title" value="<?php if(isset($save['title'])) echo $save['title']; ?>" />
+	</div>
+	<div class="col-4">
+		Parent:
+		<select name="parent">
+			<option value="-1"><?=$_SERVER['SERVER_NAME'].$this->request->relbase;?></option>
+			<optgroup label="Select Parent">
+				%subalias% <!-- this is on the nav partial at the top -->
+			</optgroup>
+		</select>
+	</div>
+	<div class="col-2">
+		Alias:
+		<input type="text" name="alias" value="<?php if(isset($save['alias'])) echo $save['alias']; ?>" />
+	</div>
+</div>
+<div class="row">
+	<div class="col-2">
+		
+	</div>
+	<div class="col-2">
+		Template: 
+		<select name="template">
+			<?=$template_select;?>
+		</select>
+	</div>
+	<div class="col-2">
+		&nbsp;
+		<button class="green">Update</button>
+	</div>
+	<div class="col-2">
+		&nbsp;
+		<a class="button" href="%appurl%">Cancel</a>
+	</div>
+</div>
+
+
+<div class="row">
 	<div class="col-3">
 		<?php include 'view/dashboard-partial-editform.php'; ?>
 	</div>
