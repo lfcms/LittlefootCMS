1b32cc12ab1fa365a5fe075b3e59d14dffbc2826
Mike <mwight90@gmail.com>
2015-11-23 21:57:47 -0500

    'fixed select boxes on Mac'

--- lf/system/template/debug.php
+++ lf/system/template/debug.php
@@ -1,21 +1,21 @@
 <!-- LF Debug Info
 
-Version: <?=$this->version;?>
+	Version: <?=$this->version;?>
 
-PHP Execution Time: <?=$exectime;?>ms
-Peak Memory Usage: <?=$memusage;?> MB
-SQL Queries: <?=$this->db->getNumQueries();?>
+	PHP Execution Time: <?=$exectime;?>ms
+	Peak Memory Usage: <?=$memusage;?> MB
+	SQL Queries: <?=$this->db->getNumQueries();?>
 
-Load Times:
+	Load Times:
 
-<?php foreach($this->timer as $function => $time): ?>
-	<?=round($time, 6)*(1000);?>ms - <?=$function;?>
-	
-<?php endforeach; ?>
+	<?php foreach($this->timer as $function => $time): ?>
+		<?=round($time, 6)*(1000);?>ms - <?=$function;?>
+		
+	<?php endforeach; ?>
 
-Included, Required files:
+	Included, Required files:
 
-<?php foreach(get_included_files() as $included): ?>
-	<?=$included;?>	
-<?php endforeach; ?>
+	<?php foreach(get_included_files() as $included): ?>
+		<?=$included;?>	
+	<?php endforeach; ?>
 -->
