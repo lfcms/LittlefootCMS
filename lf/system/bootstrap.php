357a0598e51481313636aec15eb463014f7a1ddf
Joe Still <joe@bioshazard.com>
2016-08-15 20:58:31 -0400

    order fix

--- lf/system/bootstrap.php
+++ lf/system/bootstrap.php
@@ -48,15 +48,15 @@ $cms->run();
 */
 
 require_once 'system/lib/mem.php'; // Quick memcache style key-value pair storage.
+require_once 'system/lib/cache.php'; // old mem, also does toFile()
 require_once 'system/lib/helpers.php'; 		// Helpful functions
+require_once 'system/lib/request.php'; // Parse $_SERVER['REQUEST_URI'] into usable parts
 require_once 'system/lib/orm.php'; 			// Object Relation Model base
 require_once 'system/lib/lfcss.php';			// Littlefoot css builder class
 require_once 'system/lib/user.php'; 			// user stuff
 require_once 'system/lib/install.php';		// test the install
 require_once 'system/lib/auth.php'; 			// auth stuff
-require_once 'system/lib/cache.php'; // old mem, also does toFile()
 require_once 'system/lib/plugin.php'; // Provides hooks, plugins, page request to app execution
-require_once 'system/lib/request.php'; // Parse $_SERVER['REQUEST_URI'] into usable parts
 require_once 'system/lib/acl.php'; // Tool to check user access to a request against loaded rules
 require_once 'system/lib/cms.php'; // Provides hooks, plugins, page request to app execution
 require_once 'system/lib/template.php'; // Template
