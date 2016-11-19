f8f68cbbf99743dff884e783412345359bd4af45
Joe Still <joe@bioshazard.com>
2016-11-18 22:25:23 -0500

    'index cleanup'

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
 
-require_once('lf/system/bootstrap.php');	// Bootstrap the littlefoot PHP suite.
+// Bootstrap the littlefoot libraries
+require_once('lf/system/bootstrap.php');	
 
-$cms = new \lf\cms(); 						// Could have just done (new \lf\cms)->run();,
-$cms->run();								// but I like to be able to catch if you are `PHP 5.3`.
-
-
-
-
-
-
-/* Testing without using `cms` class */
-
-//pre( \lf\www('Action'), 'var_dump' );
-
-//chdir (LF.'apps/blog');
-//include 'index.php'; // this relies on %appurl%
+// Run the full CMS application
+(new \lf\cms)->run();
