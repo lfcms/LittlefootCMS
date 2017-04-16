81767986e123dccf97e317f5cbc40083d3f17e3f
Joe Still <joe@bioshazard.com>
2017-04-15 20:46:35 -0400

    'view'

---
+++ lf/system/admin/view/home.main.php
@@ -0,0 +1,12 @@
+<div class="row">
+	<div class="col-12">
+		<h2>Home</h2>
+		<div class="row">
+			<?php foreach($widgets as $widget): ?>
+			<div class="col-4">
+				<?=$widget;?>
+			</div>
+			<?php endforeach; ?> 
+		</div>
+	</div>
+</div>
