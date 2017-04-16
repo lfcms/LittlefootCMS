24fe91e4fcf1fc355808fe50a0a617b717d418c2
Joe Still <joe@bioshazard.com>
2017-04-15 20:07:33 -0400

    'home.php added for blog widgets'

---
+++ lf/system/admin/controller/home.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace lf\admin;//\controllers;
+
+class home
+{
+	public function main()
+	{
+		$apps = scandir(LF.'apps');
+		array_shift($apps); // .
+		array_shift($apps); // ..
+		foreach($apps as $app)
+			if(is_file(LF.'apps/'.$app.'/widget.php'))
+			{
+				ob_start();
+				$oldwd = getcwd();
+				chdir(LF.'apps/'.$app);
+				include LF.'apps/'.$app.'/widget.php';
+				$widgets[] = ob_get_clean();
+				chdir($oldwd);
+			}
+		
+		include 'view/home.main.php';
+	}
+}
