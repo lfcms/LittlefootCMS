eb046c11f92f202efaebcf219cea9ed30a38a624
Joe Still <joe@bioshazard.com>
2017-04-17 00:23:43 -0400

    'media manager create. rekt. also added quick window functions to pages and blog for quick embed code access.'

---
+++ lf/system/admin/view/media.main.php
@@ -0,0 +1,101 @@
+<h2>Media</h2>
+<?=notice();?>
+
+<?php
+$parts = $this->getSubdirParts();
+$breadcount = count($parts);
+$counter = 1;
+
+
+echo '<a href="cdparent/'.$breadcount.'">Media</a> > ';
+
+foreach($parts as $part)
+{
+	if( $counter != 1 )
+		echo ' > ';
+	
+	echo '<a href="cdparent/'.($breadcount - $counter).'">'.$part.'</a>';
+	
+	$counter++;
+}
+
+
+// <?=implode(' > ', $this->getSubdirParts());
+
+
+
+?>
+
+
+
+<div class="row">
+	<div class="col-9">
+		<h3>Folder contents</h3>
+		<ul class="efvlist">
+		<?php if( $this->getSubdirDepth() > 0 ): ?>
+			<li>
+				<a href="<?=\lf\requestGet('ActionUrl');?>cdparent">..</a>
+			</li>
+		<?php endif;
+
+		foreach($files as $file): 
+			if($file == '.gitignore') continue;
+		?>
+			<li>	
+				<?php 
+				if( is_dir( $this->getFullDir().'/'.$file ) ): 
+					$file .= '/';
+					$function = 'chdir';
+				else:
+					$function = 'open';
+				endif; ?>
+				<a href="<?=\lf\requestGet('AdminUrl').'media/'.$function.'/'.$file;?>">
+					
+					
+					
+					
+					<?=$file;?>
+				</a>
+			</li>
+		<?php endforeach; ?>
+		</ul>
+	</div>
+	<div class="col-3">
+		<h3>Upload</h3>
+		<form action="<?=\lf\requestGet('ActionUrl');?>upload" method="post" enctype="multipart/form-data">
+			<ul class="vlist">
+				<li>
+					<input name="uploadedFile" class="blue button" type="file">
+				</li>
+				<li>
+					<input type="submit">
+				</li>
+			</ul>
+		</form>
+	
+	
+	
+	
+	
+	
+	
+	
+		TODO: DRAGON DROP
+		// ty http://tutorialzine.com/2011/09/html5-file-upload-jquery-php/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	</div>
+</div>
+
---
+++ lf/system/admin/view/media.open.php
@@ -0,0 +1,47 @@
+<h2>Media > File Viewer</h2>
+
+<?php
+$parts = $this->getSubdirParts();
+$breadcount = count($parts);
+$counter = 1;
+
+echo '<a href="'.\lf\requestGet('ActionUrl').'cdparent/'.$breadcount.'">Media</a> > ';
+
+foreach($parts as $part)
+{
+	if( $counter != 1 )
+		echo ' > ';
+	
+	echo '<a href="'.\lf\requestGet('ActionUrl').'cdparent/'.($breadcount - $counter).'">'.$part.'</a>';
+	
+	$counter++;
+}
+
+
+// <?=implode(' > ', $this->getSubdirParts());
+
+
+
+		$imgurl = \lf\requestGet('Subdir').'lf/media/'.$this->getSubDir().'/'.$filename;
+?>
+
+<div class="row">
+	<div class="col-12">
+		<ul class="vlist">
+			<li>
+				Copy URL
+				<input class="" type="text" value="<?=$imgurl;?>">
+				Or img tag
+				<textarea name="" id="" cols="30" rows="10"><img src="<?=$imgurl;?>" /></textarea>
+			</li>
+			<li>
+				Image Preview <br>
+				<a href="<?=$imgurl;?>"><img src="<?=$imgurl;?>" alt=""></a>
+			</li>
+			<li>
+				<a href="delete" class="red button">DELETE</a>
+			</li>
+		</ul>
+		
+	</div>
+</div>
