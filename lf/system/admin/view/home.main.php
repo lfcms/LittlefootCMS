76bee3fb0a1cf6863a1b4af33c7623aceeaf0458
Joe Still <joe@bioshazard.com>
2017-05-12 20:39:27 -0400

    'removed nav app links from dashboard for issue 193'

--- lf/system/admin/view/home.main.php
+++ lf/system/admin/view/home.main.php
@@ -1,6 +1,6 @@
 <h2 class="no_martop"><i class="fa fa-dashboard"></i> Dashboard</h2>
 <div class="row">
-	<div class="col-10">
+	<div class="col-12">
 		<div class="no_martop row">
 			<?php foreach($widgets as $app => $widget): ?>
 			<div class="col-3">
@@ -32,21 +32,5 @@
 			</div>
 			<?php endforeach; ?> 
 		</div>
-	</div>
-	<div class="col-2">
-
-		Navigation Links
-		
-		<?php foreach($links as $app => $linkArray): ?>
-		<h4><?=$app;?></h4>
-		<ul class="marbot">
-		
-		<?php foreach($linkArray as $link): ?>
-			<li>
-				<a href="<?=\lf\requestGet('AdminUrl').'navigation/id/'.$link['include'];?>"><?=$link['title'];?></a>
-			</li>
-		<?php	endforeach;  ?>
-		</ul>
-		<?php	endforeach;  ?>
 	</div>
 </div>
